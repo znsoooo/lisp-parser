6c3b44b22a8114d0ade6b2550ffb4836422d6ebe
Shixian Li <lsx7@sina.com>
2024-01-03 16:50:49 +0800

    Refactor: 面向Lisp对象搜寻位置

--- lisp.py
+++ lisp.py
@@ -1,51 +1,53 @@
 import re
 
 
-def pair(s, cp='()', d=0):
-    n1 = n2 = 0
-    for n, c in enumerate(s):
-        n1 += c == cp[0]
-        n2 += c == cp[1]
-        if n and n2 - n1 == d:
-            return n
-    raise ValueError('pair not found')
+class Lisp:
+    def __init__(self, path):
+        self.text = open(path).read()
 
+    def __getitem__(self, key):
+        return self.text[key]
 
-def find_word(s, word):
-    for m in re.finditer(r'\b%s\b' % word, s):
-        yield m.start()
+    def match(self, pos, patt):
+        return re.match(patt, self.text[pos:])
 
+    def find_word(self, word):
+        for m in re.finditer(rf'\b{word}\b', self.text):
+            yield m.start()
 
-def parent(s, pos, n):
-    return pos - pair(reversed(s[:pos]), ')(', n)
+    def find_level(self, pos, level):
+        text = self.text[pos:] if level >= 0 else ''.join(reversed(self.text[:pos]))
+        n1 = n2 = 0
+        for n, c in enumerate(text):
+            n1 += c == '('
+            n2 += c == ')'
+            if n and n2 - n1 == level:
+                return pos + n if level >= 0 else pos - n
+        else:
+            raise ValueError('pair not found')
 
 
 def find(path, string):
-    with open(path) as f:
-        s = f.read()
+    lisp = Lisp(path)
 
-    for p1 in find_word(s, string):
-        p2 = parent(s, p1, 3)
-        s2 = s[p2:]
-        if not s2.startswith('port'):
+    for p1 in lisp.find_word(string):
+        p2 = lisp.find_level(p1, -3)
+        if not lisp.match(p2, 'port'):
             continue
-        port = s2.split()[1]
-        for p3 in find_word(s, port):
-            p4 = parent(s, p3, 2)
-            s4 = s[p4:]
-            if not s4.startswith('joined'):
+        port = lisp[p2:].split()[1]
+        for p3 in lisp.find_word(port):
+            p4 = lisp.find_level(p3, -2)
+            if not lisp.match(p4, 'joined'):
                 continue
-            q4 = pair(s4, d=1)
-            joined = s4[:q4]
-            ports = re.findall('portRef (.+?) ', joined)
-            for port2 in ports:
+            q4 = lisp.find_level(p4, 1)
+            joined = lisp[p4:q4]
+            for port2 in re.findall('portRef (.+?) ', joined):
                 if port2 == port:
                     continue
-                for p5 in find_word(s, 'port +' + port2):
-                    s5 = s[p5:]
-                    q5 = pair(s5, d=1)
-                    port2_s = s5[:q5]
-                    result = re.findall(r'\(rename Simulation_Value "Simulation Value"\) +\(string "(\w+)"\)', port2_s)
+                for p5 in lisp.find_word('port +' + port2):
+                    q5 = lisp.find_level(p5, 1)
+                    port2detail = lisp[p5:q5]
+                    result = re.findall(r'\(rename Simulation_Value "Simulation Value"\) +\(string "(\w+)"\)', port2detail)
                     yield result[0]
 
 
@@ -53,7 +55,3 @@ path = '1.edn'
 string = 'D_DATA'
 
 print(list(find(path, string)))
-
-
-
-
