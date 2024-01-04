1378e322ebfc3e184d9031a1e88299059faebf33
Shixian Li <lsx7@sina.com>
2024-01-04 11:01:51 +0800

    Refactor: 解析Lisp语法结构

--- lisp.py
+++ lisp.py
@@ -1,57 +1,67 @@
 import re
 
 
-class Lisp:
-    def __init__(self, path):
-        self.text = open(path).read()
+class Node:
+    def __init__(self, parent=None, name=None):
+        self.parent = parent
+        self.name = name
+        self.args = []
+        self.level = 0 if parent is None else parent.level + 1
+        if parent:
+            parent.args.append(self)
 
-    def __getitem__(self, key):
-        return self.text[key]
-
-    def match(self, pos, patt):
-        return re.match(patt, self.text[pos:])
-
-    def find_word(self, word):
-        for m in re.finditer(rf'\b{word}\b', self.text):
-            yield m.start()
-
-    def find_level(self, pos, level):
-        text = self.text[pos:] if level >= 0 else ''.join(reversed(self.text[:pos]))
-        n1 = n2 = 0
-        for n, c in enumerate(text):
-            n1 += c == '('
-            n2 += c == ')'
-            if n and n2 - n1 == level:
-                return pos + n if level >= 0 else pos - n
+    def __add__(self, item):
+        assert isinstance(item, str), type(item)
+        if self.name is None:
+            self.name = item
         else:
-            raise ValueError('pair not found')
+            Node(self, item)
+        return self
+
+    def __getitem__(self, item):
+        assert isinstance(item, int), type(item)
+        return self.args[item]
+
+    def __repr__(self):
+        result = [self.level * '| ' + self.name if self.name else 'TOP']
+        result += [str(arg) for arg in self.args]
+        return '\n'.join(result)
 
 
-def find(path, string):
-    lisp = Lisp(path)
-
-    for p1 in lisp.find_word(string):
-        p2 = lisp.find_level(p1, -3)
-        if not lisp.match(p2, 'port'):
-            continue
-        port = lisp[p2:].split()[1]
-        for p3 in lisp.find_word(port):
-            p4 = lisp.find_level(p3, -2)
-            if not lisp.match(p4, 'joined'):
-                continue
-            q4 = lisp.find_level(p4, 1)
-            joined = lisp[p4:q4]
-            for port2 in re.findall('portRef (.+?) ', joined):
-                if port2 == port:
-                    continue
-                for p5 in lisp.find_word('port +' + port2):
-                    q5 = lisp.find_level(p5, 1)
-                    port2detail = lisp[p5:q5]
-                    result = re.findall(r'\(rename Simulation_Value "Simulation Value"\) +\(string "(\w+)"\)', port2detail)
-                    yield result[0]
+scanner = re.Scanner([
+    (r'\s+', None),
+    (r'[^"()\s]+|"[^"]*"', lambda scanner, token: ('NAME', token)),
+    (r'\(', lambda scanner, token: ('NEXT', token)),
+    (r'\)', lambda scanner, token: ('PREV', token)),
+])
 
 
-path = '1.edn'
-string = 'D_DATA'
+def parse(path):
+    # 读取文件
+    text = open(path).read()
 
-print(list(find(path, string)))
+    # 扫描全部文本
+    results, remainder = scanner.scan(text)
+
+    # 检测文本处理完毕
+    assert remainder == '', repr(remainder[:50])
+
+    # 检测括号数量匹配
+    types = [typ for typ, name in results]
+    assert types.count('NEXT') == types.count('PREV'), (types.count('NEXT'), types.count('PREV'))
+
+    # 结构化语法树
+    root = node = Node()
+    for typ, name in results:
+        if typ == 'NEXT':
+            node = Node(node)
+        elif typ == 'PREV':
+            node = node.parent
+        elif typ == 'NAME':
+            node += name
+
+    return root
+
+
+root = parse('test.edn')
+print(root)
