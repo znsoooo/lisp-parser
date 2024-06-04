51ebe7dc6f86ce2e3ce6b1981d80c2a1ff555c9b
Shixian Li <lsx7@sina.com>
2024-06-04 15:18:46 +0800

    Test: 函数内的变量名命名统一

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ Parameters:
     inst_name: R1 R2 C1
     inst_ref: &0123I4567
     port_name: IN1 IN2 OUT1 OUT2
+    port_ref: &1A &2Y
     net_name: GND +5V +3V3
 
 """
@@ -13,38 +14,46 @@ import lisp
 from pprint import pprint
 
 
-def GetName(node):
-    if 'rename' not in node.children[0]:
-        return str(node[0])
-    else:
-        return str(node[0, 1])
+def GetRefname(node):
+    node2 = node[0, 0] if node[0] == 'rename' else node[0]
+    return str(node2)
+
+
+def GetRename(node):
+    node2 = node[0, 1] if node[0] == 'rename' else node[0]
+    return str(node2)
 
 
 def GetCells(root):
-    return [GetName(cell) for cell in root['cell']]
+    table = {}
+    for cell in root['cell']:
+        table[GetRefname(cell)] = cell_table = {}
+        for port in cell['port']:
+            cell_table[GetRefname(port)] = GetRename(port)
+    return table
 
 
 def GetInstance(root):
-    insts = {}
+    table = {}
     for inst in root['instance']:
         inst_ref = str(inst[0, 0])
-        cell_name = str(inst[1, 1, 0])
+        cell_ref = str(inst[1, 1, 0])
         inst_name = str(inst[4, 0])
-        insts[inst_ref] = [cell_name, inst_name]
-    return insts
+        table[inst_ref] = [cell_ref, inst_name]
+    return table
 
 
 def GetNets(root):
-    nets = {}
+    table = {}
     for net in root['net']:
-        net_name = GetName(net)
+        net_name = GetRename(net)
         net_ports = []
         for port in net[1]:
-            port_name = str(port[0])
+            port_ref = str(port[0])
             inst_ref = str(port[1, 0])
-            net_ports.append([inst_ref, port_name])
-        nets[net_name] = net_ports
-    return nets
+            net_ports.append([inst_ref, port_ref])
+        table[net_name] = net_ports
+    return table
 
 
 path = '1.edn'
@@ -57,9 +66,10 @@ nets = GetNets(root)
 
 for net_name, net_ports in nets.items():
     for port in net_ports:
-        inst_ref, port_name = port
-        cell_name, inst_name = insts[inst_ref]
-        port[:] = [cell_name, port_name]
+        inst_ref, port_ref = port
+        cell_ref, inst_name = insts[inst_ref]
+        port_name = cells[cell_ref][port_ref]
+        port[:] = [cell_ref, port_name]
 
 for net_name, net_ports in nets.items():
     print()
